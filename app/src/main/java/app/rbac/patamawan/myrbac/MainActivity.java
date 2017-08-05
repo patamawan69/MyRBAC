597da79bcb84def6c4c0a9bfa2a06bb538d77e26
admin <patamawan69@gmail.com>
2017-08-05 15:53:39 +0700

    05082560

--- app/src/main/java/app/rbac/patamawan/myrbac/MainActivity.java
+++ app/src/main/java/app/rbac/patamawan/myrbac/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //Add Fragment
         if(savedInstanceState == null){
             MainFragment mainFragment = new MainFragment();
